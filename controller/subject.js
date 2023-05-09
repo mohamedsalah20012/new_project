05bd77a70f32d15843244c12c0a0d1dbffe33c01
OsamaIbrahim1 <oi5921435@gmail.com>
2023-05-09 19:05:56 +0300

    'Added'

---
+++ controller/subject.js
@@ -0,0 +1,33 @@
+import subject from "../models/subject.js";
+import department from "../models/department.js";
+
+export const index = async (req, res) => {
+  const subjects = await subject.find({}, { name: 1 }).lean();
+
+  res.render("subjects/index", { subjects });
+};
+
+export const create = async (req, res) => {
+  const departments = await department.find().lean();
+  console.log(departments);
+  res.render("subjects/create", { departments });
+};
+
+export const store = async (req, res) => {
+  const { name, code, department } = req.body;
+  await subject.create({
+    name,
+    code,
+    department,
+  });
+
+  res.render("/subjects");
+};
+
+export const show = async (req, res) => {
+  const { id } = req.params;
+
+  const singleSubject = await subject.findById(id).populate('department').lean();
+
+  res.render("subjects/show", { subject: singleSubject });
+};
--- server.js
+++ server.js
@@ -2,16 +2,26 @@ import express from "express";
 import { engine } from "express-handlebars";
 import dotenv from "dotenv";
 dotenv.config();
-import mongoose from "mongoose";
 
+import mongoose from "mongoose";
 mongoose.connect(process.env.mongoConnectionUrl);
 
-const app = express();
+import subjectsRouter from "./routes/subjects.js";
 
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./tamplates");
 
+app.use("/subjects", subjectsRouter);
+
+// .then(() => {
+
+// });
+
 app.listen(process.env.port, () => {
-  console.log(`started the application on http://localhost:${process.env.port}`);
+  console.log(
+    `started the application on http://localhost:${process.env.port}`
+  );
 });
